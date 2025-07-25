c783132cfdc2117dcea53d54d9bb211c8b5e7797
Sourabhp-2003 <sp666920@example.com>
2025-07-25 19:56:04 +0530

    first commit

---
+++ JS/External_js2.js
@@ -0,0 +1,4 @@
+ /* this is an external js.*/
+ function display() {
+        document.write("Hello i am learning External Javascript from beginig. and this document.write only for clients.")
+        }
